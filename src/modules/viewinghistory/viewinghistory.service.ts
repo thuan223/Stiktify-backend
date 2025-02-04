d58654b9610c7034947a8663cff2bbb4e89b750f
thuan223 <thuantnn.it@gmail.com>
2025-02-04 17:16:59 +0700

    Sort ViewingHistory

--- src/modules/viewinghistory/viewinghistory.service.ts
+++ src/modules/viewinghistory/viewinghistory.service.ts
@@ -8,16 +8,16 @@ import aqp from 'api-query-params';
 
 @Injectable()
 export class ViewinghistoryService {
-   constructor(
-     @InjectModel(ViewingHistory.name)
-     private viewingHistoryModel: Model<ViewingHistory>,
-   ) {}
+  constructor(
+    @InjectModel(ViewingHistory.name)
+    private viewingHistoryModel: Model<ViewingHistory>,
+  ) {}
 
-   async create(createViewinghistoryDto: CreateViewinghistoryDto) {
+  async create(createViewinghistoryDto: CreateViewinghistoryDto) {
     const { userId } = createViewinghistoryDto;
-    
+
     const count = await this.viewingHistoryModel.countDocuments({ userId });
-  
+
     if (count >= 100) {
       await this.viewingHistoryModel
         .findOneAndDelete({ userId }, { sort: { createdAt: 1 } })
@@ -27,31 +27,37 @@ export class ViewinghistoryService {
     const newHistory = new this.viewingHistoryModel(createViewinghistoryDto);
     return await newHistory.save();
   }
-  
-  async handleGetListViewingHistory(query: string, current: number, pageSize: number) {
+
+  async handleGetListViewingHistory(
+    query: string,
+    current: number,
+    pageSize: number,
+  ) {
     const { filter, sort } = aqp(query);
     // Thêm điều kiện lọc theo userId
     if (filter.current) delete filter.current;
     if (filter.pageSize) delete filter.pageSize;
-  
+
     if (filter.query) {
-      filter.query = JSON.parse(filter.query);  // Chuyển chuỗi JSON thành đối tượng
+      filter.query = JSON.parse(filter.query); // Chuyển chuỗi JSON thành đối tượng
     }
 
     if (!current) current = 1;
     if (!pageSize) pageSize = 10;
-  
+
     // Tính tổng số lượng
-    const totalItems = await this.viewingHistoryModel.countDocuments(filter.query);
+    const totalItems = await this.viewingHistoryModel.countDocuments(
+      filter.query,
+    );
     // Tính tổng số trang
     const totalPages = Math.ceil(totalItems / pageSize);
     const skip = (current - 1) * pageSize;
     const result = await this.viewingHistoryModel
-    .find(filter.query)  // filter đã bao gồm userId
-    .limit(pageSize)
-    .skip(skip)
-    .populate('videoId') // Lấy thông tin chi tiết của video
-    .sort(sort as any);
+      .find(filter.query) // filter đã bao gồm userId
+      .limit(pageSize)
+      .skip(skip)
+      .populate('videoId') // Lấy thông tin chi tiết của video
+      .sort({ createdAt: -1, ...sort });
     return {
       meta: {
         current, // trang hiện tại
@@ -62,7 +68,7 @@ export class ViewinghistoryService {
       result,
     };
   }
-  
+
   findAll() {
     return `This action returns all viewinghistory`;
   }
@@ -78,5 +84,4 @@ export class ViewinghistoryService {
   remove(id: number) {
     return `This action removes a #${id} viewinghistory`;
   }
- 
 }
